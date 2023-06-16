c17bce3d66752ccb21747aa2b9962cec2335ce78
Harsh Maurya <103075283+harsmaur@users.noreply.github.com>
2023-06-16 20:50:26 +0530

    Home Component

---
+++ src/Components/home/HeaderSocials.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className="home__socials">
+
+        <a href="https://www.linkedin.com/in/harsh-maurya-041a58231/" className="home__social-link" rel="noopener" target='_blank'>
+        <i class="fa-brands fa-linkedin"></i>
+        </a>
+
+        <a href="https://github.com/harsmaur" className="home__social-link" rel="noopener" target='_blank'>
+        <i class="fa-brands fa-github"></i>
+        </a>
+
+        <a href="https://twitter.com/harsmaur" className="home__social-link" rel="noopener" target='_blank'>
+        <i class="fa-brands fa-twitter"></i>
+        </a>
+
+        <a href="https://www.instagram.com/harsmaur/" className="home__social-link" rel='noopener' target='_blank'>
+        <i class="fa-brands fa-instagram"></i>
+        </a>
+    </div>
+  )
+}
+
+export default HeaderSocials
--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './Home.css'
+import Me from '../../assets/avatar-1.svg'
+import HeaderSocials from './HeaderSocials'
+import ScrollDown from './ScrollDown'
+import Shapes from './Shapes'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="home container">
+
+
+      <div className="intro">
+        <img src={Me} alt="" className="home__img" />
+        <h1 className="home__name">Harsh Maurya</h1>
+        <span className="home__education">I'm a Front-End Developer</span>
+        
+        <HeaderSocials/>
+        <a href="#contact" className="btn">Contact Me</a>
+
+
+        <ScrollDown/>
+      </div>
+     
+     <Shapes/>
+    </section>
   )
 }
 
