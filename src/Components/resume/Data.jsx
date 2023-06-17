c615448ad6ae906bacd094889613a53709930f4d
Harsh Maurya <103075283+harsmaur@users.noreply.github.com>
2023-06-17 14:34:02 +0530

    Resume Component

---
+++ src/Components/resume/Data.jsx
@@ -0,0 +1,52 @@
+const Data = [
+    {
+      id: 1,
+      category: "education",
+      icon: "icon-graduation",
+      year: "2021-present",
+      title: "Bachelor's Degree(Btech)",
+      desc: "Madhav Institute Of Technology And Science Gwalior",
+    },
+    {
+      id: 2,
+      category: "education",
+      icon: "icon-graduation",
+      year: "2020",
+      title: "Higher Secondary",
+      desc: "Santhome Higher Sec School Maihar",
+    },
+    {
+      id: 3,
+      category: "education",
+      icon: "icon-graduation",
+      year: "2018",
+      title: "High School",
+      desc: "Santhome Higher Sec School Maihar",
+    },
+    {
+      id: 4,
+      category: "experience",
+      icon: "icon-briefcase",
+      year: "",
+      title: "Web Designer",
+      desc: "Lorem ipsum dolor sit amet quo ei simul congue exerci  ",
+    },
+    {
+      id: 5,
+      category: "experience",
+      icon: "icon-briefcase",
+      year: "2013 - 2017",
+      title: "Front-End Developer",
+      desc: "Lorem ipsum dolor sit amet quo ei simul congue exerci  .",
+    },
+    {
+      id: 6,
+      category: "experience",
+      icon: "icon-briefcase",
+      year: "2009 - 2013",
+      title: "Back-End Developer",
+      desc: "Lorem ipsum dolor sit amet quo ei simul congue exerci a",
+    },
+  ];
+
+export default Data
