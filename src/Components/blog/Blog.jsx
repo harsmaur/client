a79f34eefc12817cb266b7f5c217e320cd5df50a
Harsh Maurya <103075283+harsmaur@users.noreply.github.com>
2023-06-19 16:13:26 +0530

    Blog Component

--- src/Components/blog/Blog.jsx
+++ src/Components/blog/Blog.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
 import './Blog.css'
+
+import BlogData from './BlogData';
+
+
+
 const Blog = () => {
   return (
-    <div>Blog</div>
+
+    <section className="blog container section" id="blog">
+
+      <h2 className="section__title">Latest Posts</h2>
+      <div className="blog__container grid">
+        {BlogData.map(({ id, title, image, link, category, date }) => {
+          return (
+            <div className="blog__card" key={id}>
+              <div className="blog__thumb">
+                <a href={link} ><span className="blog__category">{category}</span></a>
+                <a href={link}><img src={image} alt="" className="blog__img" /></a>
+              </div>
+              <div className="blog__details">
+                <h3 className="blog__title">{title}</h3>
+                <div className="blog__meta">
+                  <span className="blog__dot">.</span>
+                  <span>{date}</span>
+                </div>
+              </div>
+            </div>
+          )
+        })}
+
+
+      </div>
+    </section>
+
   )
 }
 
---
+++ src/Components/blog/BlogData.jsx
@@ -0,0 +1,36 @@
+
+
+import image1 from '../../assets/blog-1.svg';
+import image2 from '../../assets/blog-2.svg';
+import image3 from '../../assets/blog-3.svg';
+const BlogData = [
+    {
+        id: 1,
+        title: 'How Secured PAYMENT Works',
+        image: image1,
+        link: 'http://localhost:3000/#blog',
+        category: 'lorem',
+        date: '2387432'
+
+    },
+    {
+        id: 2,
+        title: 'Black holes discovery',
+        image: image2,
+        link: 'http://localhost:3000/#blog',
+        category: 'lorem',
+        date: '8137982164'
+
+    },
+    {
+        id: 3,
+        title: 'Black holes',
+        image: image3,
+        link: 'http://localhost:3000/#blog',
+        category: 'lorem',
+        date: '912749874'
+
+    },
+]
+
+export default BlogData
