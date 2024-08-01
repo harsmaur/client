b5abc9ff62f5476e75afbf77afb55dcccae47931
Harsh Maurya <103075283+harsmaur@users.noreply.github.com>
2024-08-01 22:32:33 +0530

    resume  jsx updated undefined return from map

--- src/Components/resume/Resume.jsx
+++ src/Components/resume/Resume.jsx
@@ -1,39 +1,39 @@
-import React from 'react'
-import './Resume.css'
-import Data from './Data'
-import Card from './Card'
+import React from 'react';
+import './Resume.css';
+import Data from './Data';
+import Card from './Card';
+
 const Resume = () => {
   return (
-
     <section className="resume container section" id="resume">
-
       <h2 className="section__title">Education and Programming Experience</h2>
       <div className="resume__container grid">
-
+        
         <div className="timeline grid">
           {Data.map((val, id) => {
             if (val.category === 'education') {
               return (
                 <Card key={id} icon={val.icon} title={val.title} year={val.year} desc={val.desc} />
-              )
+              );
             }
+            return null;  // Ensure a value is returned
           })}
         </div>
-
 
         <div className="timeline grid">
           {Data.map((val, id) => {
             if (val.category === 'experience') {
               return (
                 <Card key={id} icon={val.icon} title={val.title} year={val.year} desc={val.desc} />
-              )
+              );
             }
+            return null;  // Ensure a value is returned
           })}
         </div>
 
       </div>
     </section>
-  )
+  );
 }
 
-export default Resume
+export default Resume;
